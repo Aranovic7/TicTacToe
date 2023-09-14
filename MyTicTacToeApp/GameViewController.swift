c7a794d88f9349fc1ba8fa45951a47ed978de469
Aran Ali <aliaran2717@gmail.com>
2023-09-14 17:30:23 +0200

    UPDATE: My TicTacToe App can now check for winner New problem is that currentPlayer does not switch, the game does not switch players

--- MyTicTacToeApp/GameViewController.swift
+++ MyTicTacToeApp/GameViewController.swift
@@ -15,7 +15,6 @@ class GameViewController: UIViewController {
     @IBOutlet weak var c3: UIButton!
     
     var currentPlayer: Int = 1
-    var currentGameStatus: String = "X"
     var isPlaying = false
     var playerOneName: String?
     var playerTwoName: String?
@@ -31,15 +30,18 @@ class GameViewController: UIViewController {
         if (currentPlayer == 1) {
             turnSign.text = playerOneName
         } else if (playerOneName == "") {
-            turnSign.text = "X"
+            turnSign.text = "Player X turn"
             
         }
+        if (currentPlayer == 2) {
+            turnSign.text = playerTwoName
+        } else if (playerTwoName == "") {
+            turnSign.text = "Player O turn"
+            }
         
-    }
-    
-  
-    
-    
+        
+        }
+        
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         let index = sender.tag // knapparna har en tagg som motsvarar deras position i boardArray
@@ -54,6 +56,42 @@ class GameViewController: UIViewController {
             
             currentPlayer = 3 - currentPlayer
         }
+        
+        if let winner = checkForWinner() {
+            turnSign.text = winner
+        } else {
+            currentPlayer = 3 - currentPlayer
+            if (currentPlayer == 1) {
+                turnSign.text = playerOneName
+            } else {
+                turnSign.text = playerTwoName
+            }
+                
+        }
+        
+        
     }
+    
+    
+    func checkForWinner() -> String? {
+        let winningPatterns: [[Int]] = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Horisontalt
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Verticalt
+            [0, 4, 8], [2, 4, 6] // Diagonalt
+        ]
+        for pattern in winningPatterns {
+            let positions = pattern.map { boardArray[$0] }
+            if positions == ["X", "X", "X"] {
+                return "Player One win!"
+            } else if positions == ["O", "O", "O"] {
+                return "Player two win!"
+            }
+        }
+        if boardArray.contains("") {
+            return nil
+        } else {
+            return "Oavgjort!"
+        }
     }
-
+    
+}
